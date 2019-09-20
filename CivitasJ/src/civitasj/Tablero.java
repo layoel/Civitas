5c8933cbd4ed9becb83cd70b43fb20f74429f471
ecastillo <ecastillo@ei142062>
2019-09-20 11:28:17 +0200

    inicio practica

---
+++ CivitasJ/src/civitasj/Tablero.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitasj;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ecastillo
+ */
+public class Tablero {
+    /*los atributos son pivados*/
+    private int numCasillaCarcel;
+    private  ArrayList<Casilla> casillas;
+    private int porSalida;
+    private Boolean tieneJuez;
+    
+    private static String nombre;
+    
+    tablero(String c);
+    
+    
+}
