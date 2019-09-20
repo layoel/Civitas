1e515c5b39c1fa7fd096af7db799351ad1fe7d8d
ecastillo <ecastillo@ei142062>
2019-09-20 11:09:53 +0200

    inicio practica

--- CivitasR/lib/civitas/estadosjuego.rb
+++ CivitasR/lib/civitas/estadosjuego.rb
@@ -2,7 +2,6 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-module Civitas
-  puts Tipocasilla::CALLE
-  # puts 'hola'
+module Estadosjuego
+    
 end
