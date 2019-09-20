3afe25caad8c291effbec916e54d617724533b5e
ecastillo <ecastillo@ei142062>
2019-09-20 10:45:01 +0200

    inicio practica

---
+++ CivitasJ/src/civitasj/TipoSorpresa.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package civitasj;
+
+/**
+ *
+ * @author ecastillo
+ */
+public enum TipoSorpresa { IRCARCEL, IRCASILLA, PAGARCOBRAR, PORCASAHOTEL, PORJUGADOR, SALIRCARCEL}
