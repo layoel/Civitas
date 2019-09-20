688205afdbfcb27c457f48c8ead0252a34d5e579
ecast <ecast@172.20.80.254>
2019-09-20 12:26:39 +0200

    inicio practica

--- CivitasJ/src/civitasj/Tablero.java
+++ CivitasJ/src/civitasj/Tablero.java
@@ -19,8 +19,61 @@ public class Tablero {
     private Boolean tieneJuez;
     
     private static String nombre;
+    /**
+     * Constructor
+     * @param c indice de la casilla de la carcel
+    */
+    Tablero(int c){
+        
+        if (c >=1 )
+            numCasillaCarcel = c;
+        else
+            numCasillaCarcel = 1;
+        
+        casillas = new ArrayList<Casilla>();
+        casillas.add(Salida);
+        porSalida = 0;
+        tieneJuez = false; 
+    }
     
-    tablero(String c);
+    Boolean correcto(){
+    
+    }
+    
+    Boolean correcto(int numCasilla){
+    
+    }
+    
+    int getCarcel(){
+        return numCasillaCarcel;
+    }
+    
+    int getPorSalida(){
+    
+    }
+    
+    void aniadeCasilla(){
+    
+    }
+    
+    void aniadeJuez(){
+    
+    }
+    
+    Casilla getCasilla(int numCasilla){
+       
+    }
+    
+    int nuevaPosicion(int actual, int tirada){
+    
+    }
+    
+    int calcularTirada(int origen, int destino){
+    
+    }
+    
+    
+            
     
     
 }
