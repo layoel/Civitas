6f11a4743bdcf3e61d0f0c1dc5a5b34867337e4f
ecast <ecast@172.20.80.254>
2019-09-20 18:08:16 +0200

    inicio practica

--- CivitasJ/src/civitas/Tablero.java
+++ CivitasJ/src/civitas/Tablero.java
@@ -11,6 +11,17 @@ import java.util.ArrayList;
  *
  * @author ecastillo
  */
+
+/**
+ *@brief Representa el tablero del juego imponiendo restricciones 
+ * sobre el mismo en las reglas del juego
+ * 
+ * \param numCasillaCarcel el numero de la casilla donde se encuentrala carcel
+ * \param casillas contenedor de las casillas del juego
+ * \param porSalida numero de veces que se ha pasado por la salida en un turno
+ * \param tieneJuez si el tablero tiene o no casilla de ese tipo
+ */
+
 public class Tablero {
     /*los atributos son pivados*/
     private int numCasillaCarcel;
@@ -19,11 +30,16 @@ public class Tablero {
     private Boolean tieneJuez;
     
     private static String nombre;
+   
     /**
-     * Constructor
-     * @param c indice de la casilla de la carcel
+     * @brief Constructor
+     * \param c indice de la casilla de la carcel
     */
+    
     Tablero(int c){
+        
+        Casilla Salida;
+        Salida = new Casilla("Salida");
         
         if (c >=1 )
             numCasillaCarcel = c;
@@ -31,6 +47,9 @@ public class Tablero {
             numCasillaCarcel = 1;
         
         casillas = new ArrayList<Casilla>();
+        
+       
+        
         casillas.add(Salida);
         porSalida = 0;
         tieneJuez = false; 
