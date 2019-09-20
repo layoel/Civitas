8f5df6942875fe51bb655dd6c710704822aefbd5
ecast <ecast@172.20.80.254>
2019-09-20 19:32:40 +0200

    inicio practica

--- CivitasJ/src/civitas/Tablero.java
+++ CivitasJ/src/civitas/Tablero.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 public class Tablero {
     /*los atributos son pivados*/
     private int numCasillaCarcel;
-    private  ArrayList<Casilla> casillas;
+    private ArrayList<Casilla> casillas;
     private int porSalida;
     private Boolean tieneJuez;
     
@@ -48,6 +48,7 @@ public class Tablero {
         //creo una casilla llamada Salida
         Casilla Salida;
         Salida = new Casilla("Salida");
+  
         //añádo salida a casillas
         casillas.add(Salida);
        
@@ -55,44 +56,92 @@ public class Tablero {
         tieneJuez = false; 
     }
     
+    
+    /**
+     * @brief Consulta si el numero de elementos en casillas es mayor que el indice
+     * de la casilla de la carcel y que se dispone de una casilla tipo juez
+     * @return true si el tablero es correcto y puede usarse para jugar
+     */
     Boolean correcto(){
     
     }
     
+    
+    /**
+     * @brief comprieba si el tablero es correcto para jugar 
+     * @param numCasilla indice válido para acceder a elementos de casillas
+     * @return true si se puede jugar y el indice es valido
+     */
     Boolean correcto(int numCasilla){
     
     }
     
+    
+    /**
+     * @brief Consultor de numero de casilla carcel
+     * @return numCasillaCarcel
+     */
     int getCarcel(){
         return numCasillaCarcel;
     }
     
+    
+    
+    /**
+     * @brief numero de veces que se pasa por salida
+     * @return porSalida
+     */
     int getPorSalida(){
     
     }
     
-    void aniadeCasilla(){
     
+    /**
+     * @brief añade a casillas la casilla c
+     * @param c casilla que se va a añadir
+     */
+    void añadeCasilla( Casilla c){
+        
     }
     
-    void aniadeJuez(){
+    /**
+     * @brief añade la casilla juez si no esta aun
+     */
+    void añadeJuez(){
     
     }
-    
+   
+     /**
+     * @brief Comprueba si la casilla esta en el tablero
+     * @return casilla si es correcto o null si no lo es
+     */
     Casilla getCasilla(int numCasilla){
        
     }
     
+    
+    /**
+     * @brief calcula la posicion en el tablero a partir de la actual y avanza 
+     * una tirada de unidades
+     * @param actual
+     * @param tirada
+     * @return -1 si el tablero no es correcto, posicion 
+     */
     int nuevaPosicion(int actual, int tirada){
     
     }
     
+    
+    /**
+     * @brief calcula el numero de casillas que avanza 
+     * @param origen casilla donde estoy
+     * @param destino casilla a la que llego
+     * @return tirada de dado que se habria tenido que obtener para ir desde la 
+     * casilla origen a la casilla destino 
+     */
     int calcularTirada(int origen, int destino){
     
-    }
-    
-    
-            
+    }            
     
     
 }
