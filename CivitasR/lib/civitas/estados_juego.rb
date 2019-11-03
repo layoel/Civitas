6c18958f42e090b68d7cb70963d08a46c93bc9ae
Elvira Castillo <ecastillo@correo.ugr.es>
2019-11-03 23:26:32 +0100

    cambio tabulaciones

--- CivitasR/lib/civitas/estados_juego.rb
+++ CivitasR/lib/civitas/estados_juego.rb
@@ -3,8 +3,8 @@ module Civitas
     
 		INICIO_TURNO = :inicio_turno  
 		DESPUES_CARCEL = :despues_carcel
-  		DESPUES_AVANZAR = :despues_avanzar
-  		DESPUES_COMPRAR = :despues_comprar
-  		DESPUES_GESTIONAR = :despues_gestionar
+    DESPUES_AVANZAR = :despues_avanzar
+    DESPUES_COMPRAR = :despues_comprar
+    DESPUES_GESTIONAR = :despues_gestionar
   end
 end
