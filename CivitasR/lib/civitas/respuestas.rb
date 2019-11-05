dfdd3b0b128737f828955a3558f408e704771f7f
Elvira Castillo <ecastillo@correo.ugr.es>
2019-11-05 21:29:04 +0100

    Implementacion correcta

--- CivitasR/lib/civitas/respuestas.rb
+++ CivitasR/lib/civitas/respuestas.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -7,5 +9,5 @@ module Civitas
     NO = :no
     SI = :si
   end
-  Lista_Respuestas = [Respuestas::NO, Respuestas::SI] 
+ 
 end
