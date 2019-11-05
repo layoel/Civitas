fafe3aa90d77809dd33e1a3da6a3f8c753d491c8
Elvira Castillo <ecastillo@correo.ugr.es>
2019-11-05 21:25:09 +0100

    Implementacion correcta

---
+++ CivitasJ/src/juegoTexto/GestionesInmobiliarias.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegoTexto;
+
+/**
+ *
+ * @author ELVIRA
+ */
+public enum GestionesInmobiliarias {
+    VENDER,
+    HIPOTECAR,
+    CANCELAR_HIPOTECA,
+    CONSTRUIR_CASA,
+    CONSTRUIR_HOTEL,
+    TERMINAR
+}
--- CivitasR/lib/civitas/gestiones_inmobiliarias.rb
+++ CivitasR/lib/civitas/gestiones_inmobiliarias.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -11,10 +13,5 @@ module Civitas
     CONSTRUIR_HOTEL = :construir_hotel
     TERMINAR = :terminar
   end
-  Lista_Operaciones = [OperacionesInmobiliarias::VENDER,
-    OperacionesInmobiliarias::HIPOTECAR,
-    OperacionesInmobiliarias::CANCELAR_HIPOTECA,
-    OperacionesInmobiliarias::CONSTRUIR_CASA,
-    OperacionesInmobiliarias::CONSTRUIR_HOTEL,
-    OperacionesInmobiliarias::TERMINAR]
+
 end
